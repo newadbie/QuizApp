89742c82725d61569bcad496d51dc376780a761e
Adrian Bielec <bielecadriandeveloper@gmail.com>
2020-08-21 18:08:48 +0200

    First project commit

---
+++ QuizApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuizApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
