23a31b8432408a8265765b441ff76642fbf89ae0
Adrian Bielec <bielecadriandeveloper@gmail.com>
2020-09-19 13:06:56 +0200

    Use EF, save data into database to avoid repeating create request.

--- QuizAPI/Startup.cs
+++ QuizAPI/Startup.cs
@@ -21,7 +21,7 @@ namespace QuizAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<QuizContext>(opt => opt.UseInMemoryDatabase("Quiz"));
+            services.AddDbContext<QuizContext>(opt => opt.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Application2DB;Trusted_Connection=True;"));
             services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
